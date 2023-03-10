748bc83d0e9eeb77002e6f4d4d2ff3ef14c0cfa8
mota <thiagomotape@hotmail.com.br>
2023-03-10 20:53:07 -0300

    update
    fiz a conexão do produto com o backend

--- techshop/src/app/produtos/produtos.component.ts
+++ techshop/src/app/produtos/produtos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduto, produtos } from '../produtos';
+import { ProdutosService } from '../produtos.service';
 
 @Component({
   selector: 'app-produtos',
@@ -7,12 +8,12 @@ import { IProduto, produtos } from '../produtos';
   styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent implements OnInit {
-  produtos: IProduto[] = produtos;
+  produtos: IProduto[] | undefined = produtos;
 
-  constructor() { }
+  constructor(private produtosService: ProdutosService) { }
 
   ngOnInit(): void {
-      
+      this.produtos = this.produtosService.getAll();
   }
 
 }
